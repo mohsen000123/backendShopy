0bbb2b52cbece541c8a7e261d38511862258cbda
cant111 <mohsenbahmani000123@gmail.com>
2024-07-21 15:19:34 +0330

    add access fron cors

--- routes/middlewares/auth.js
+++ routes/middlewares/auth.js
@@ -1,36 +1,40 @@
-const jwt = require('jsonwebtoken');
-const db = require('../../app/db/createDatabase');
+const jwt = require("jsonwebtoken");
+const db = require("../../app/db/createDatabase");
 
 const auth = (req, res, next) => {
-    const token = req.headers?.authorization;
+  const token = req.headers?.authorization || req.cookies?.shopy_token;
 
-    if(! token ) {
-        return res.status(403).json({ status: 'fail' , message : 'unauthorized'})
-    }
+  if (!token) {
+    return res.status(403).json({ status: "fail", message: "unauthorized" });
+  }
 
-    try {
-        let decoded = jwt.verify(token, process.env.TOKEN_KEY);
-        console.log(decoded);
-        db.get(`SELECT * FROM users WHERE id = ? and token = ?` , [ decoded.user_id , token] , function(err, user) {
-            
-            if (err) {
-                res.status(400).json({"error": err.message})
-                return;
-            }
+  try {
+    let decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    console.log(decoded);
+    db.get(
+      `SELECT * FROM users WHERE id = ? and token = ?`,
+      [decoded.user_id, token],
+      function (err, user) {
+        if (err) {
+          res.status(400).json({ error: err.message });
+          return;
+        }
 
-            if(user.token != token) {
-                return res.status(403).json({ status: 'fail' , message : 'unauthenticated'})
-            }
+        if (user.token != token) {
+          return res
+            .status(403)
+            .json({ status: "fail", message: "unauthenticated" });
+        }
 
-            const { id , name , email } = user;
-            req.user = { id , name , email }
-            next()
-        });
-    } catch(err) {
-        console.log(err);
-        return res.status(403).json({ status: 'fail' , message : 'unauthenticated'})
-    }
-}
-
+        const { id, name, email } = user;
+        req.user = { id, name, email };
+        next();
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(403).json({ status: "fail", message: "unauthenticated" });
+  }
+};
 
 module.exports = auth;
